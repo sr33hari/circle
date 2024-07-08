ce00199fe40bfd53eb19c6f5f0886e26d5991765
sr33hari <sreehari98@gmail.com>
2024-07-08 09:15:20 -0700

    terraformv3.1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project     = "circle-425516"
-  region      = "us-west2"
+  project = "circle-425516"
+  region  = "us-west2"
 }
 
 resource "google_storage_bucket" "my_bucket" {
