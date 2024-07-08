9d055171447d998779b28d7a23d5be112b4b34ec
sr33hari <sreehari98@gmail.com>
2024-07-08 09:13:45 -0700

    terraformv3

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+provider "google" {
+  project     = "circle-425516"
+  region      = "us-west2"
+}
+
 resource "google_storage_bucket" "my_bucket" {
   name                     = "circle-terraform-bucket"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+  project                  = "circle-425516"
 }
